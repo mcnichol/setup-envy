6b5d92eb059b94bed749243bfe9aade08f203850
McNichol <mcnichol.m@gmail.com>
2017-02-16 19:25:40 -0600

    Initial script for server setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+apt-get -y install git
+git config --global user.name "McNichol"
+git config --global user.email mcnichol.m@gmail.com
+
+apt-get -y install tmux
+mv tmux.conf ~/.tmux.conf
+
+apt-get -y install vim
+mv vimrc ~/.vimrc
+
+apt-get -y install zsh
+mv zshrc ~/.zshrc
+
 
